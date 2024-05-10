a1fbb376e3d6440ad4d49f91487004812731e33a
ParkJiHyeon <91263647+parkjihyeon124594@users.noreply.github.com>
2024-05-10 15:00:51 +0900

    first commit

---
+++ src/main/java/webprogrammingTeam/matchingService/MatchingServiceApplication.java
@@ -0,0 +1,13 @@
+package webprogrammingTeam.matchingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webprogrammingTeam/matchingService/MatchingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package webprogrammingTeam.matchingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
