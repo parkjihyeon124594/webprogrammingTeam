61f5b2cb920df92b782296fd019cd8b94567d8f2
kalteon <102862962+kalteon@users.noreply.github.com>
2024-06-10 10:13:41 +0900

    feat: add ApiUtil to MessageController

--- src/main/java/webprogrammingTeam/matchingService/domain/message/controller/MessageController.java
+++ src/main/java/webprogrammingTeam/matchingService/domain/message/controller/MessageController.java
@@ -2,11 +2,13 @@ package webprogrammingTeam.matchingService.domain.message.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import webprogrammingTeam.matchingService.domain.message.dto.MessageDTO;
 import webprogrammingTeam.matchingService.domain.message.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import webprogrammingTeam.matchingService.global.util.ApiUtil;
 
 import java.util.List;
 
@@ -25,17 +27,17 @@ public class MessageController {
     // 같은 채팅채널 id를 가진 메세지들을 반환. isPublic이 ture면 그냥 주고, isPublic이 false면 오류.
     @GetMapping("/public_channel/{channelId}")
     @Operation(summary = "공개 채널의 모든 메세지 조회", description = "공개 채널의 모든 메세지를 조회하는 로직, 채널 입장시 사용")
-    public ResponseEntity<List<MessageDTO>> getAllMessagesByPublicChannelId(@PathVariable Long channelId) {
+    public ResponseEntity<ApiUtil.ApiSuccessResult<List<MessageDTO>>> getAllMessagesByPublicChannelId(@PathVariable Long channelId) {
         List<MessageDTO> allMessages = messageService.findAllMessageByPublicChannelId(channelId);
-        return ResponseEntity.ok().body(allMessages);
+        return ResponseEntity.ok().body(ApiUtil.success(HttpStatus.OK, allMessages));
     }
 
     @GetMapping("/private_channel/{channelId}/member/{memberId}")
     @Operation(summary = "비밀 채널의 모든 메세지 조회", description = "비밀 채널의 모든 메세지를 조회하는 로직, 채널 입장시 사용")
-    public ResponseEntity<List<MessageDTO>> getAllMessagesByPrivateChannelId(@PathVariable Long channelId,
+    public ResponseEntity<ApiUtil.ApiSuccessResult<List<MessageDTO>>> getAllMessagesByPrivateChannelId(@PathVariable Long channelId,
                                                                              @PathVariable Long memberId) {
         List<MessageDTO> allMessages = messageService.findAllMessageByPrivateChannelId(channelId, memberId);
-        return ResponseEntity.ok().body(allMessages);
+        return ResponseEntity.ok().body(ApiUtil.success(HttpStatus.OK, allMessages));
     }
 
     // post 없음. message handler가 메세지 송신과 수신을 처리함.
@@ -46,14 +48,16 @@ public class MessageController {
     // 메세지 id로 메세지 삭제 (공개, 비공개 나눠야 할 수도 있음)
     @DeleteMapping("/{messageId}")
     @Operation(summary = "메세지 하나 삭제", description = "하나의 메세지를 삭제하는 로직")
-    public void deleteMessage(@PathVariable Long messageId) {
+    public ResponseEntity<ApiUtil.ApiSuccessResult<?>> deleteMessage(@PathVariable Long messageId) {
         messageService.delete(messageId);
+        return ResponseEntity.ok().body(ApiUtil.success(HttpStatus.OK));
     }
 
     // 같은 채팅채널 id를 가진 메세지들을 삭제
     @DeleteMapping("/channel/{channelId}")
     @Operation(summary = "채널의 모든 메세지 삭제", description = "하나의 채널에 있는 모든 메세지를 삭제하는 로직")
-    public void deleteAllMessagesByChannelId(@PathVariable Long channelId) {
+    public ResponseEntity<ApiUtil.ApiSuccessResult<?>> deleteAllMessagesByChannelId(@PathVariable Long channelId) {
         messageService.deleteAllMessageByChannelId(channelId);
+        return ResponseEntity.ok().body(ApiUtil.success(HttpStatus.OK));
     }
 }
