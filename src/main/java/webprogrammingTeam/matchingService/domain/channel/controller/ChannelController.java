93a127bacc7cd151e00389f352156da8461d04b5
kalteon <102862962+kalteon@users.noreply.github.com>
2024-06-10 10:11:34 +0900

    feat: add ApiUtil to ChannelController

--- src/main/java/webprogrammingTeam/matchingService/domain/channel/controller/ChannelController.java
+++ src/main/java/webprogrammingTeam/matchingService/domain/channel/controller/ChannelController.java
@@ -2,12 +2,14 @@ package webprogrammingTeam.matchingService.domain.channel.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
 import webprogrammingTeam.matchingService.domain.channel.dto.ChannelTitleDTO;
 import webprogrammingTeam.matchingService.domain.channel.dto.CreateChannelRequest;
 import webprogrammingTeam.matchingService.domain.channel.entity.Channel;
 import webprogrammingTeam.matchingService.domain.channel.service.ChannelService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import webprogrammingTeam.matchingService.global.util.ApiUtil;
 
 import java.util.List;
 
@@ -20,30 +22,31 @@ public class ChannelController {
 
     @GetMapping("/titles")
     @Operation(summary = "모든 공개 채널 조회", description = "모든 공개 채널을 조회하는 로직")
-    public ResponseEntity<List<ChannelTitleDTO>> getAllPublicChannelsTitles() {
+    public ResponseEntity<ApiUtil.ApiSuccessResult<List<ChannelTitleDTO>>> getAllPublicChannelsTitles() {
         List<ChannelTitleDTO> channelTitles =  channelService.getAllPublicChannelTitles();
-        return ResponseEntity.ok().body(channelTitles);
+        return ResponseEntity.ok().body(ApiUtil.success(HttpStatus.OK,channelTitles));
     }
 
     @PostMapping("/public")
     @Operation(summary = "공개 채널 생성", description = "공개 채널을 생성하는 로직")
-    public ResponseEntity<Long> createPublicChannel(@RequestBody CreateChannelRequest request) {
+    public ResponseEntity<ApiUtil.ApiSuccessResult<Long>> createPublicChannel(@RequestBody CreateChannelRequest request) {
         Channel publicChannel = channelService.createPublicChannel(request.getTitle());
-        return ResponseEntity.ok().body(publicChannel.getChannelId());
+        return ResponseEntity.ok().body(ApiUtil.success(HttpStatus.OK, publicChannel.getChannelId()));
     }
 
     @PostMapping("/private")
     @Operation(summary = "비밀 채널 생성", description = "비밀 채널을 생성하는 로직")
-    public ResponseEntity<Long> createPrivateChannel(@RequestBody CreateChannelRequest request) {
+    public  ResponseEntity<ApiUtil.ApiSuccessResult<Long>> createPrivateChannel(@RequestBody CreateChannelRequest request) {
         Channel privateChannel = channelService.createPrivateChannel(request.getTitle());
-        return ResponseEntity.ok().body(privateChannel.getChannelId());
+        return ResponseEntity.ok().body(ApiUtil.success(HttpStatus.OK, privateChannel.getChannelId()));
     }
 
     // 채팅방 id로 채팅방 삭제 (단 실수를 방지하기 위해 연관된 메세지와 구독이 존재하면 삭제 거부 예외 구현해야함)
     @DeleteMapping("/{channelId}")
     @Operation(summary = "채널 삭제", description = "채널을 삭제하는 로직, 관련 메세지와 구독 삭제 필요")
-    public void deleteChat(@PathVariable Long channelId) {
+    public ResponseEntity<ApiUtil.ApiSuccessResult<?>> deleteChat(@PathVariable Long channelId) {
         channelService.deleteChannel(channelId);
+        return ResponseEntity.ok().body(ApiUtil.success(HttpStatus.OK));
     }
 
 }
